44a0b99ba9b029db073bce19b74a22e82a415f40
Naoki-png <ct.anao38@outlook.jp>
2022-01-19 23:19:43 +0900

    コメントアウト削除

--- src/functions/authfun.ts
+++ src/functions/authfun.ts
@@ -4,12 +4,6 @@ import * as dateFns from "date-fns";
 admin.initializeApp();
 
 export const createUser = functions.auth.user().onCreate(async (user) => {
-  // await admin.firestore().collection("users").add({
-  //   userId: user.uid,
-  //   userName: user.displayName,
-  //   userIcon: user.photoURL,
-  // });
-
   await admin.firestore().collection("users").doc(user.uid).set({
     userId: user.uid,
     userName: user.displayName,

ec532c3a2b058cee47b409459bfef59cada3a7a6
Naoki-png <ct.anao38@outlook.jp>
2022-01-19 23:02:05 +0900

    日付をString型にした

--- src/functions/authfun.ts
+++ src/functions/authfun.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as dateFns from "date-fns";
 admin.initializeApp();
 
 export const createUser = functions.auth.user().onCreate(async (user) => {
@@ -14,7 +15,7 @@ export const createUser = functions.auth.user().onCreate(async (user) => {
     userName: user.displayName,
     userIcon: user.photoURL,
     userIsLogin: true,
-    created: Date(),
-    updated: Date(),
+    created: dateFns.format(new Date(), "yyyy-MM-dd HH:mm:ss"),
+    updated: dateFns.format(new Date(), "yyyy-MM-dd HH:mm:ss"),
   });
 });

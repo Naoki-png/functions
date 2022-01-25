82ff0ba51979bc9c2c439123bd55924fa5bbb37f
Naoki-png <ct.anao38@outlook.jp>
2022-01-25 21:41:25 +0900

    東京のタイムゾーンで日付を使うようにした

--- src/functions/authfun.ts
+++ src/functions/authfun.ts
@@ -1,15 +1,22 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as dateFns from "date-fns";
+import * as dateFnsTz from "date-fns-tz";
 admin.initializeApp();
 
+const TIME_ZONE_TOKYO = "Asia/Tokyo";
+const FORMAT_YMDHMS = "yyyy-MM-dd HH:mm:ss";
+
 export const createUser = functions.auth.user().onCreate(async (user) => {
+  const dateAtTokyo = dateFnsTz.utcToZonedTime(new Date(), TIME_ZONE_TOKYO);
+  const formattedDate = dateFns.format(dateAtTokyo, FORMAT_YMDHMS);
+
   await admin.firestore().collection("users").doc(user.uid).set({
     userId: user.uid,
     userName: user.displayName,
     userIcon: user.photoURL,
     userIsLogin: true,
-    created: dateFns.format(new Date(), "yyyy-MM-dd HH:mm:ss"),
-    updated: dateFns.format(new Date(), "yyyy-MM-dd HH:mm:ss"),
+    created: formattedDate,
+    updated: formattedDate,
   });
 });

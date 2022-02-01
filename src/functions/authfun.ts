3e834d06a53ba67c9679ec75fe6c520414b8d1f0
Naoki-png <ct.anao38@outlook.jp>
2022-02-01 22:57:35 +0900

    ストレージのファイルを削除できるように修正

--- src/functions/authfun.ts
+++ src/functions/authfun.ts
@@ -6,6 +6,7 @@ admin.initializeApp();
 
 const TIME_ZONE_TOKYO = "Asia/Tokyo";
 const FORMAT_YMDHMS = "yyyy-MM-dd HH:mm:ss";
+const BUCKET_NAME = "pien-e74c4.appspot.com";
 
 export const createUser = functions.auth.user().onCreate(async (user) => {
   const dateAtTokyo = dateFnsTz.utcToZonedTime(new Date(), TIME_ZONE_TOKYO);
@@ -19,4 +20,27 @@ export const createUser = functions.auth.user().onCreate(async (user) => {
     created: formattedDate,
     updated: formattedDate,
   });
+});
+
+export const deleteUser = functions.auth.user().onDelete(async (user) => {
+  // usersテーブルからの削除
+  await admin.firestore().collection("users").doc(user.uid).delete();
+
+  // postsテーブルからの削除
+  const postSnapshot = await admin.firestore().collection("posts")
+      .where("userId", "==", user.uid).get();
+  postSnapshot.forEach((doc) => {
+    doc.ref.delete();
+  });
+
+  // favoritesテーブルからの削除
+  const favoriteSnapshot = await admin.firestore().collection("favorites")
+      .where("favoriteUserId", "==", user.uid).get();
+  favoriteSnapshot.forEach((doc) => {
+    doc.ref.delete();
+  });
+
+  // storageの削除
+  await admin.storage().bucket(BUCKET_NAME)
+      .deleteFiles({prefix: `${user.uid}`});
 });
